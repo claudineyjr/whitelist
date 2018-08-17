5eb08e2d4fae9221ccd1febcd91b8d0ea647e4ac
Claudiney Junior <claudiney.alves@fatec.sp.gov.br>
2018-08-17 14:47:26 -0300

    Ensure that some informations are mandatory

--- models/user.js
+++ models/user.js
@@ -2,13 +2,19 @@ const conn = require('database/connection/mongo');
 const Schema = conn.mongoose.Schema;
 
 const UserSchema = new Schema({
-  createdAt: { type: Date, default: Date.now },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
   userId: {
     type: String,
     required: true,
     unique: true,
   },
-  data: Object
+  data: {
+    type: Object,
+    required: true,
+  }
 }, { collection: 'Users' });
 
 module.exports = conn.model('User', UserSchema);
